6092ce2a47d6cf34d0327df42d1034a215e7addc
dev5socializus <115540262+dev5socializus@users.noreply.github.com>
2023-09-14 08:38:08 +0200

    adding searchbar

--- App.js
+++ App.js
@@ -1,9 +1,11 @@
 import { ScrollView, StyleSheet, Text, View } from 'react-native';
 import SquareComponent from './component/card';
 import { useEffect, useState } from 'react';
+import { SearchBar } from 'react-native-elements';
 
 export default function App() {
   const [data, setData] = useState([])
+  const [search, setSearch] = useState('')
 
   useEffect(() => {
     fetch('https://pokebuildapi.fr/api/v1/pokemon/limit/10')
@@ -13,10 +15,21 @@ export default function App() {
 
   return (
     <View style={styles.container}>
+      <SearchBar
+        placeholder="Search a pokemon..."
+        onChangeText={(text) => (setSearch(text))}
+        value={search}
+        platform="android" 
+        cancelButtonTitle="Cancel"
+        style={{marginTop: 10}}
+      />
       <ScrollView>
-        {data.map((pokemon, index) => (
-          <SquareComponent data={pokemon} key={index}/>
-        ))}
+        {data.map((pokemon, index) => {
+          const name = pokemon.name
+          if (search === '' || name.startsWith(search)) {
+            return (<SquareComponent data={pokemon} key={index}/>)
+          }
+        })}
       </ScrollView>
     </View>
   );
